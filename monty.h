29427a8a14cefd55fcc79ceea46c505b2aeb2836
kinyarasam <samkinyara2000@gmail.com>
2022-06-16 21:15:51 +0300

    monty.h

--- monty.h
+++ monty.h
@@ -1,6 +1,21 @@
 #ifndef _MONTY_H
 #define _MONTY_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#define STACK 0
+#define QUEUE 1
+#define DELIMS " \n\t\a\b"
+
+/* GLOBAL OPCODE TOKENS */
+extern char **op_toks;
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -30,5 +45,14 @@ typedef struct instruction_s
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+/* PRIMARY INTERPRETOR FUNCTIONS */
+int init_stack(stack_t **stack);
+int check_mode(stack_t *stack);
+void free_stack(stack_t **stack);
+int monty(FILE *script_fd);
+unsigned int token_arr_len(void);
+void free_tokens(void);
+void set_op_tok_error(int error_code);
 
 #endif /* _MONTY_H */
